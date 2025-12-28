58a02e7db02ce8f2f9e3f0e401eb5b4acb40efb4
Samir Reyes <samir1604@gmail.com>
2025-12-27 20:51:01 -0500

    fix(ui): Added padding bottom to open Transfermovil button

--- lib/src/core/extensions/buildcontext_extension.dart
+++ lib/src/core/extensions/buildcontext_extension.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 extension BuildcontextExtension on BuildContext {
   Size get getSize => MediaQuery.sizeOf(this);
 
-  double get getStatusBarHeight => MediaQuery.paddingOf(this).top;
+  double get systemTopPadding => MediaQuery.viewPaddingOf(this).top;
+
+  double get systemBottomPadding => MediaQuery.viewPaddingOf(this).bottom;
 
   Offset getCenterOfBody(double appBarHeight) {
-    final availableHeight = getSize.height - appBarHeight - getStatusBarHeight;
+    final availableHeight =
+        getSize.height - appBarHeight - systemTopPadding - systemBottomPadding;
     return Offset(getSize.width / 2, availableHeight / 2);
   }
 }
